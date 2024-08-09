efe3a7c00a31c38b4c6542193b504604c212f8ae
SaiDeepakKumarReddy <ksaideepak123@gmail.com>
2024-08-08 23:53:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar />} />
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/products' element={<Products/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
---
+++ src/components/pages/Products.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../../App.css';
+export default function Products(){
+    return <h1 className='services'>PRODUCTS</h1>
+}
